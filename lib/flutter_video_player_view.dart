40e1022455d043b2f635ec0c6b7bdc46d950b940
student9128 <jingpengchn@163.com>
2023-08-24 15:47:54 +0800

    remove:remove useless code.

--- lib/flutter_video_player_view.dart
+++ lib/flutter_video_player_view.dart
@@ -4,33 +4,14 @@ import 'package:flutter_float_window/flutter_float_window.dart';
 
 class FlutterVideoPlayerView extends StatelessWidget {
   const FlutterVideoPlayerView(
-      {Key? key,
-        required this.videoUrl,
-        this.title = "",
-        this.artist = "",
-        this.coverUrl = "",
-        this.position = 0,
-        this.speed=1.0})
+      {Key? key})
       : super(key: key);
-  final String videoUrl;
-  final String title;
-  final String artist;
-  final String coverUrl;
-  final int position;
-  final double speed;
 
   @override
   Widget build(BuildContext context) {
     return UiKitView(
         viewType: "flutter_video_player_view",
-        creationParams: <String, dynamic>{
-          "videoUrl": videoUrl,
-          "title": title,
-          "artist": artist,
-          "coverUrl": coverUrl,
-          "position": position,
-          "speed":speed
-        },
+        creationParams: const <String, dynamic>{},
         creationParamsCodec: const StandardMessageCodec(),
         onPlatformViewCreated: (viewId) {
         });
